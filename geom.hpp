d2898ee07341c7833ff644808b3e15d3414c4932
rumen tsenkov <rumen.tsenkov@gmail.com>
2021-11-10 09:25:20 +0200

    Rabota v chas

--- geom.hpp
+++ geom.hpp
@@ -34,23 +34,25 @@ struct Line {
     Line(double A = 0, double B = 0, double C = 0) : A(A), B(B), C(C) {}
 
     Line(const Point& p1, const Point& p2) {
-        A = 0; B = 0; C = 0;
+        B = -1;
+        A = (p1.y-p2.y)/(p1.x-p2.x); 
+        C = p1.y - A*p1.x;
     }
 
     bool parallel(const Line& other) const {
-        return true;
+        return eq((this->A/other.A),(this->B/other.B));
     }
 
     Line parallel(const Point& p) {
-        return Line(0,0,0);
+        return Line(this->A,this->B,((this->A*p.x) + (this->B*p.y)));
     }
 
     bool perpendicular(const Line& other) const {
-        return true;
+        return  eq((this->A/-other.B),(this->B/other.A));
     }
 
     Line perpendicular(const Point& p) {
-        return Line(0,0,0);
+        return Line(-this->B,this->A,((this->B*p.x)-(this>A*p.y)));
     }
 
     void print(ostream& out) const {
